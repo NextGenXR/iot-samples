a8f31d705555d26c2e7ce1011f99feb913fff628
NextGenXR <jenni@nextgenxr.com>
2023-11-09 16:37:08 -0600

    Script to check connection to server

---
+++ source/ingest_app_csv/login.py
@@ -0,0 +1,12 @@
+import omni.client
+
+server_url = "omniverse://simready.ov.nvidia.com"
+# server_url = "omniverse://localhost"
+
+# Attempt to access a resource on the server
+status, response = omni.client.stat(server_url)
+
+if status == omni.client.Result.OK:
+    print(f"Successfully connected to the server {server_url}")
+else:
+    print(f"Failed to connect to the server {server_url}.")
